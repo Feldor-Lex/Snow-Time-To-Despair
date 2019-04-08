3c14d349757a667bb23976ff04e04934f068d613
Unknown <xel2754@gmail.com>
2019-04-08 13:12:26 +1200

    Setting up Zone Fields

--- Zone.java
+++ Zone.java
@@ -1,4 +1,21 @@
+import ecs100.*;
+import java.awt.color.*;
+import java.util.*;
 
 public class Zone {
-
+	//FIELDS
+	private ArrayList<Zone> adjacentZones = new ArrayList<>();//Any zones which the current zone is connected to.
+	private boolean isBreached;//Whether or not the current zone is breached! Simple enough.
+	private double health;//A measure of how badly damaged the zone is.
+	private double damageThreshold; 
+	
+	
+	
+	
+	public String setToString() {
+		
+		
+		
+	}
 }
+
