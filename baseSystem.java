0f32c75264ccb24873a56fb32cf70e0630bc5c20
Feldor55 <xel2754@gmail.com>
2019-04-10 22:07:07 +1200

    Added mock-up map added class for base system

---
+++ baseSystem.java
@@ -0,0 +1,16 @@
+import ecs100.*;
+import java.awt.color.*;
+import java.util.*;
+
+public class baseSystem {
+	//FIELDS
+	private String name; //The name of the system in question, eg: 
+	
+	
+	
+	
+	
+	
+	
+
+}
--- runSnowstorm.java
+++ runSnowstorm.java
@@ -6,9 +6,12 @@ public class runSnowstorm{
 	
 	//CONSTANTS
 	public static final boolean isInEditMode=false;//Whether or not the simulation is in active use mode, or being tweaked.
+	public static final double passiveStormDamageMult = 1;
+
 	
 	//FIELDS
 	private ArrayList<Zone> Zones = new ArrayList<>();
+	private String displayMode = "Map"; //Current rendering mode. Can be "Map", "Profiles", "Info"
 	
 	
 	public void initialize() {//Loads any saved data for the current map
@@ -16,23 +19,31 @@ public class runSnowstorm{
 		
 	}
 	
-
 	
+	
+/*
+ * Draws the graphical map once everything else is done. Must be called after ANY changes, even panning slightly.
+ */
+	public void draw() {
+		
+		
+
+	}
 
 	
 	/**
-	 * 
 	 * Main method. At present, it initializes the UI, configures the UI so that it displays only the graphics pane, then 
-	 * 
+	 * runs a handful of test functions.
 	 */
 	public static void main(String[] args) {
 		UI.initialise();
 		UI.setDivider(0);//Sets it so that there is no graphics output region - ONLY
 		runSnowstorm KG3 = new runSnowstorm();
 		KG3.initialize();//Sets up all of the data about the current environment.
+		
 		Zone testZone = new Zone();
 		UI.println(testZone.toString());
-		
+		for(int i=0; i<100; i++) {UI.println((int)(Math.random()*10));}
 		
 		
 		
